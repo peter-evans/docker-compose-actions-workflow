525862574cb8775c2454ece079b50082d40a5676
chander1411 <leonardotoala1411@gmail.com>
2022-11-30 17:33:12 -0500

    cambios

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen for {} times.\n'.format(count)
